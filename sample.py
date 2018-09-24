aaeb5b2c678a5250fef8a8caa4052e8d5662cf38
Ryo Yamamoto <ry2m2t@gmail.com>
2018-09-24 21:37:54 +0900

    use gensim word2vec to explore similar words

---
+++ sample.py
@@ -0,0 +1,12 @@
+# First download the pretrained gensim model:
+# $ wget http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip
+# $ unzip latest-ja-word2vec-gensim-model.zip
+
+# from collections import defaultdict
+from gensim.models.word2vec import Word2Vec
+
+model = Word2Vec.load('word2vec.gensim.model')
+
+some_words = ['ブランド', 'バッグ', 'アルマーニ']
+for w in some_words:
+    print(model.wv.most_similar(w))
